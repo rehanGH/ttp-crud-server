3e49daa5bf0a6249efaa90bd36163c61bfb50b5a
Rehan Khan <rehangh8924@gmail.com>
2020-06-14 16:03:34 -0400

    edited the students.js to add pu

--- database/models/student.js
+++ database/models/student.js
@@ -3,6 +3,12 @@ const db = require("../db");
 
 const Student = db.define("student", {
   firstName: { type: Sequelize.STRING, allowNull: false },
+  address: { type: Sequelize.STRING, allowNull: false },
+    imageUrl: {
+      type: Sequelize.STRING,
+      defaultValue: "https://via.placeholder.com/480x240?text=Placeholder",
+    },
+    description: { type: Sequelize.TEXT, defaultValue: "", allowNull: false },
 });
 
 module.exports = Student;
